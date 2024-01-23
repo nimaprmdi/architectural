d43dea635ed2c65f708fcf0b732697b03a019ebf
Nima Prmdi <nimaprmdi@gmail.com>
2024-01-23 07:33:52 +0330

    Fix minor issues

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,20 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import ProjectItems from "./projects/items";
 import ProjectsHeader from "./projects/ProjectsHeader";
 
-const Gallery = ({ hasCategory = false }) => {
+//
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+//
+const Gallery = ({ data, hasCategory = false }) => {
+  const [images, setImages] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setImages(data);
+  }, [data]);
+
   return (
     <section className="c-projects  flex flex-wrap justify-center mb-16 mt-16">
       <ProjectsHeader hasCategory={hasCategory} title="Gallery" />
 
       <div className="c-projects__items w-full flex justify-between flex-wrap items-start max-w-screen-2xl">
-        <ProjectItems />
-        <ProjectItems />
-        <ProjectItems />
+        <button onClick={() => setOpen(true)}>Open Lightbox</button>
 
-        <ProjectItems />
-        <ProjectItems />
-        <ProjectItems />
+        {data && data.length > 0 ? (
+          <Lightbox open={open} close={() => setOpen(false)} slides={images} />
+        ) : (
+          <>
+            <ProjectItems />
+            <ProjectItems />
+            <ProjectItems />
+
+            <ProjectItems />
+            <ProjectItems />
+            <ProjectItems />
+          </>
+        )}
       </div>
     </section>
   );
